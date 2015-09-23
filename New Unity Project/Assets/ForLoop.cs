abb4041734decd4ca2f034a620d3e421ce6e3527
Jessie Graham <bunnygirl1200@hotmail.com>
2015-09-23 15:11:50 -0600

    more projects

--- New Unity Project/Assets/ForLoop.cs
+++ New Unity Project/Assets/ForLoop.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class ForLoop : MonoBehaviour 
 
 {
-	int numEnemeies = 3;
+	int numEnemies = 3;
 
 	void Start()
 	{
 		for (int i = 0; i < numEnemies; i++) 
 		{
-			Debug.Log ("Creating enemy number: " + i)
+			Debug.Log ("Creating enemy number: " + i);
 		}
 	}
 
---
+++ New Unity Project/Assets/IfStatementPassOff.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatementPassOff : MonoBehaviour 
+{
+	float hotchocTemperature = 50.0f;
+	float hotLimitTemperature = 70.0f;
+	float coldLimitTemperature = 40.0f;
+
+	void Start()
+	{
+		if (hotchocTemperature > hotLimitTemperature)
+		{
+			print("The Hot Chocolate is too hot!");
+		}
+		else if (hotchocTemperature < coldLimitTemperature)
+		{
+			print("The Hot Chocolate is too cold!");
+		}
+		else
+		{
+			print("The Hot Chocolate is just right.");
+		}
+	}		
+}
